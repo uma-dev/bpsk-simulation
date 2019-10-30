86cc3cf5e50a8af14b7cfb574f69cabadcb138c6
Omar Roldan <omar.roldan.50@gmail.com>
2019-10-30 09:07:54 -0600

    First OOP version usable with graphs

--- bpsk-encode.py
+++ bpsk-encode.py
@@ -3,13 +3,14 @@ from math import sin, pi
 import numpy
 import matplotlib.pyplot as plt
 
-
 #---------------------------------------------------------------------------------------------#
+
 def randPolarNRZSource( numberOfBits ):
     return [ 2*randint(0,1)-1 for i in range(numberOfBits) ]
 
 #---------------------------------------------------------------------------------------------#
-class BPSKEncode:
+
+class BPSK:
     def __init__(self, listNRZ, amp, cycles, n):
         self.listNRZ = listNRZ
         self.amp = amp
@@ -21,64 +22,56 @@ class BPSKEncode:
 
     def getNRZSource(self):
         return self.listNRZ
-
-    def expandBits(self):
-        self.expandedList =  [ item for item in self.listNRZ for i in range(self.n) ]
-    
+ 
     def getExpandedSource(self):
         return self.expandedList
-
-    def carrierGen(self):
-        self.carrier = [ self.amp*round(sin(2*pi*self.cycles*round(i,4)), 5) for i in numpy.arange(0 , 1 , 1.0/self.n) ]
     
     def getCarrier(self):
         return self.carrier
 
-    def multiply(self):
+    def encode(self):
+        self.expandedList =  [ item for item in self.listNRZ for i in range(self.n) ]
+        self.carrier = [ self.amp*round(sin(2*pi*self.cycles*round(i,4)), 5) for i in numpy.arange(0 , 1 , 1.0/self.n) ]
         c = (len(self.expandedList)/(len(self.carrier) )  *self.carrier )             #Multiple carriers in all sequence
         self.output = [ c[i]*self.expandedList[i] for i in range(len(self.expandedList)) ]
 
     def getOutput(self):
         return self.output
+
+    def plotEncoding(self):
+        t = [i for i in numpy.arange(0,len(self.listNRZ), 1.0/(self.n*1)) ]
+        fig, axs = plt.subplots(4,1,constrained_layout=True)
+        axs[0].set_xlim(-0.5,len(self.listNRZ)+0.5)
+        axs[0].set_ylim(-0.1,1.2)
+        axs[0].plot( [i+0.5 for i in range(len(self.listNRZ))], [(i+1)/2 for i in self.listNRZ], 'ro' )
+        axs[0].vlines( [i+0.5 for i in range(len(self.listNRZ))],0, [(i+1)/2 for i in self.listNRZ], color='red')
+        axs[0].set_ylabel('Bitstream')
+        axs[1].plot( t, self.expandedList, 'orange')
+        axs[1].set_ylabel('NRZ')
+        axs[2].plot( t, self.output, 'darkblue', t, self.expandedList, 'orange' )
+        axs[2].set_ylabel('Superposicion')
+        axs[3].plot( t, self.output, 'darkblue' )
+        axs[3].set_ylabel('BPSK')
+        fig.suptitle('Proceso de transmision', fontsize=16)
+        plt.show()
+
 #---------------------------------------------------------------------------------------------#
 
-nBits  = 10             #number of bits of the source
-amp    = 0.8            #amplitude of carrier
-cycles = 3              #cycles per bit
-n      = cycles*20      #number of samples per bit
+nBits   = 10                                 #number of bits of the source
+amp     = 0.8                                #amplitude of carrier
+cycles  = 3                                  #cycles per bit
+n       = cycles*20                          #number of samples per bit
+randSrc = randPolarNRZSource(nBits)          #get a random list or NRZ values
 
-randomSource = randPolarNRZSource(nBits)
-print(randomSource)
+BPSK = BPSK(randSrc, amp, cycles, n)
+BPSK.encode()
+BPSK.plotEncoding()
 
-BPSK = BPSKEncode(randomSource, amp, cycles, n)
-#print BPSK.getNRZSource()
-BPSK.expandBits()
-#print BPSK.getExpandedSource()
-BPSK.carrierGen()
-#print BPSK.getCarrier() 
-BPSK.multiply()
 #print BPSK.getOutput()
+#print BPSK.getNRZSource()
+#print BPSK.getExpandedSource()
+#print BPSK.getCarrier() 
 
-#t = [i for i in numpy.arange(0,nBits, 1.0/(n*1)) ]
-#fig, axs = plt.subplots(4,1,constrained_layout=True)
-
-#axs[0].set_xlim(-0.5,nBits+0.5)
-#axs[0].set_ylim(-0.1,1.2)
-#axs[0].plot( [i+0.5 for i in range(len(source))], [(i+1)/2 for i in source], 'ro' )
-#axs[0].vlines( [i+0.5 for i in range(len(source))],0, [(i+1)/2 for i in source], color='red')
-#axs[0].set_ylabel('Bitstream')
-
-#axs[1].plot( t, expandedSource, 'orange')
-#axs[1].set_ylabel('NRZ')
-
-#axs[2].plot( t, output, 'darkblue', t, expandedSource, 'orange' )
-#axs[2].set_ylabel('Superposicion')
-
-#axs[3].plot( t, output, 'darkblue' )
-#axs[3].set_ylabel('BPSK')
-
-#fig.suptitle('Proceso de transmision', fontsize=16)
-#plt.show()
 
 
 
